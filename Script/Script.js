bab2e5987135e3c0fc9fe08e58d3b134aba17ac7
ALEXANDRE WILLIANS DA SILVA <alexandresilva004@cs.cruzeirodosul.edu.br>
2026-03-27 20:50:46 -0300

    Atualizacao

---
+++ Script/Script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let slides = document.querySelectorAll(".slide");
+    let index = 0;
+    let dotsContainer = document.querySelector(".dots");
+
+    slides.forEach((_, i) => {
+        let dot = document.createElement("span");
+        dot.addEventListener("click", () => showSlide(i));
+        dotsContainer.appendChild(dot);
+    });
+
+    let dots = document.querySelectorAll(".dots span");
+
+    function showSlide(i) {
+        slides[index].classList.remove("active");
+        dots[index].classList.remove("active");
+
+        index = i;
+
+        slides[index].classList.add("active");
+        dots[index].classList.add("active");
+    }
+
+    document.querySelector(".next").onclick = () => {
+        let i = (index + 1) % slides.length;
+        showSlide(i);
+    };
+
+    document.querySelector(".prev").onclick = () => {
+        let i = (index - 1 + slides.length) % slides.length;
+        showSlide(i);
+    };
+
+    setInterval(() => {
+        let i = (index + 1) % slides.length;
+        showSlide(i);
+    }, 5000);
+
+    showSlide(0);
+
+});
